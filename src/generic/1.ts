5bf814c611f87a3c4a9370948a5acb9091c46f90
VButenk0 <vladflash10@gmail.com>
2024-01-03 23:26:20 +0200

    hw2 done

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,12 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+function getPromise<T extends string | number>(): Promise<T[]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(["Text" as T, 50 as T]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then((data: (string | number)[]) => {
   console.log(data);
 });
-
-export {};
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,23 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+type TopType = Pick<AllType, "name" | "color">;
+
+type BottomType = Pick<AllType, "position" | "weight">;
+
+function compare<T extends TopType, U extends BottomType>(
+  top: T,
+  bottom: U
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
